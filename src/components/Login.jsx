6a20e464a619081083fa99281aeeb03bd63d6691
Coder-life25 <prajeetshahlac@gmail.com>
2025-03-26 01:59:35 +0545

    connecting frontend and backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,33 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log({ email, password });
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5555/login",
+        { email, password },
+        { withCredentials: true }
+      );
+      navigate("/");
+      console.log(res.data);
+      console.log({ email, password });
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   return (
     <form
-      onSubmit={handleLogin}
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
       className="max-w-md mx-auto p-4 my-20 shadow-md rounded-lg bg-white"
     >
       <h2 className="text-xl font-bold mb-4">Login</h2>
@@ -32,6 +48,7 @@ const Login = () => {
       <button
         type="submit"
         className="w-full bg-blue-500 text-white p-2 rounded"
+        onClick={handleLogin}
       >
         Login
       </button>
